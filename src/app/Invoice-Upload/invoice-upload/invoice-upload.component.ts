068518614429686df9bb48d66ae426378b8a5c29
“m84288955” <moaaz.salah.eldesoky1@huawei-partners.com>
2024-01-22 17:04:14 +0200

    stable point on the app

--- src/app/Invoice-Upload/invoice-upload/invoice-upload.component.ts
+++ src/app/Invoice-Upload/invoice-upload/invoice-upload.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-invoice-upload',
   templateUrl: './invoice-upload.component.html',
   styleUrls: ['./invoice-upload.component.css']
 })
 export class InvoiceUploadComponent {
-  file:any;
+  file:any ;
+  // file:any = {name:"test file",size:"30M"};
   http: any;
+  constructor(private router:Router){}
   getFile(e:any){
     this.file = e?.target.files[0];
     console.log('file',this.file)
@@ -18,6 +20,6 @@ export class InvoiceUploadComponent {
 
   //   //call API
   //   this.http.post('......URL.....',formData).subscribe((res)=>{})
-  // }
-}
+  this.router.navigate(['/invoicemgm'])
+  }
 }
