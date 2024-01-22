1ce6f675dc931b35bbe35bfbc5a86c163ab8867a
“m84288955” <moaaz.salah.eldesoky1@huawei-partners.com>
2024-01-22 15:43:20 +0200

    add some functionality on the invoice upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,12 @@ import { InvoiceManagementComponent } from './Invoice Management/invoice-managem
 import { InvoiceUploadComponent } from './Invoice-Upload/invoice-upload/invoice-upload.component';
 
 const routes: Routes = [
+  {path:"",component:InvoiceUploadComponent},
+  {path:"invoiceupl",component:InvoiceUploadComponent},
+  {path:"invoicemgm",component:InvoiceManagementComponent},
   {path:"login", component:LoginComponent},
   {path:"register",component:RegisterComponent},
-  {path:"dashbord",component:DashboardComponent},
-  {path:"invoiceupl",component:InvoiceUploadComponent},
-  {path:"invoicemgm",component:InvoiceManagementComponent}
+  {path:"dashbord",component:DashboardComponent}
 ];
 
 @NgModule({
